61056a91af3d6b5e4a7364f60aa8c88c34e2e8e8
Andreas Alminde <alminde7@hotmail.com>
2020-04-10 20:26:20 +0200

    Created statusBarItem with content length shown in decimal and hex. Added command to copy HEX value to clipboard, when clicking on statusBarItem.

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,49 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+let statusBarItem: vscode.StatusBarItem;
+
 export function activate(context: vscode.ExtensionContext) {
+	const commandId = 'alminde.convert-document-length-to-hex'
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "document-to-hex" is now active!');
+	context.subscriptions.push(vscode.commands.registerCommand(commandId, () => {
+		const contentLength = calculateHexFromDocument();
+		if(contentLength) {
+			vscode.env.clipboard.writeText(contentLength.hex);
+			vscode.window.showInformationMessage(`Copied ${contentLength.hex} to clipboard`);
+		}
+	}));
+	
+	statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right);
+	statusBarItem.command = commandId;
+	context.subscriptions.push(statusBarItem);
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('document-to-hex.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+	context.subscriptions.push(vscode.window.onDidChangeActiveTextEditor(updateStatusBar));
+	context.subscriptions.push(vscode.window.onDidChangeTextEditorSelection(updateStatusBar));
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Document-to-hex!');
-	});
+	updateStatusBar();
+}
 
-	context.subscriptions.push(disposable);
+function updateStatusBar(): void {
+	const contentLength = calculateHexFromDocument();
+	if(contentLength) {
+		statusBarItem.text = `DEC:${contentLength?.decimal}, HEX:${contentLength?.hex}`;
+		statusBarItem.show();
+	} else {
+		statusBarItem.hide();
+	}
+}
+
+
+function calculateHexFromDocument(): ContentLength | undefined {
+	const editor = vscode.window.activeTextEditor;
+	if(editor) {
+		const contentLength = editor.document.getText().length;
+		return new ContentLength(contentLength, contentLength?.toString(16));
+	}
+}
+
+class ContentLength {
+	constructor(public decimal: number, public hex: string) { }
 }
 
 // this method is called when your extension is deactivated
